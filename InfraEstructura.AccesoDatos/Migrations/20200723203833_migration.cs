605c3ee7b5fb3d33f4b3931d1906909b365dcc61
ksilva <ksilva@dgi.gob.ni>
2020-07-23 15:28:27 -0600

    nueva tabla Log con nuevo esquema

---
+++ InfraEstructura.AccesoDatos/Migrations/20200723203833_migration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InfraEstructura.AccesoDatos.Migrations
+{
+    public partial class migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "AUDI");
+            
+            migrationBuilder.CreateTable(
+                name: "LogTransaccion",
+                schema: "AUDI",
+                columns: table => new
+                {
+                    LogTransaccionID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Usuario = table.Column<string>(type: "varchar(200)", nullable: false),
+                    FechaProceso = table.Column<DateTime>(nullable: false),
+                    Accion = table.Column<string>(type: "varchar(200)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LogTransaccion", x => x.LogTransaccionID);
+                });
+
+          
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+           
+        }
+    }
+}
